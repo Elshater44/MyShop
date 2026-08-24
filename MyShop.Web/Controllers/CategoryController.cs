133167c4678e349234d6df7dcdc04122ae330dad
ElshaterITI <elshaterhassan@tutamail.com>
2026-08-24 19:16:54 +0300

    Add project files.

---
+++ MyShop.Web/Controllers/CategoryController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyShop.DataAccess.Data;
+using MyShop.Entities.Models;
+
+namespace MyShop.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+            _context.Categories.Add(category);
+            TempData["Create"] = "Category has been created successfully";
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == 0 || id == null) return NotFound();
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+            _context.Categories.Update(category);
+            TempData["Update"] = "Category has been updated successfully";
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == 0 || id == null) return NotFound();
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteCategory(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (category is null) return NotFound();
+            _context.Categories.Remove(category);
+            TempData["Delete"] = "Category has been deleted successfully";
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
